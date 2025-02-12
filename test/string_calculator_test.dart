9facdb6c45a01f4cdd3cae38b2d9a3288365c042
tushar-ventura <48640828+trick4u@users.noreply.github.com>
2025-02-12 16:32:09 +0530

    4tests

--- test/string_calculator_test.dart
+++ test/string_calculator_test.dart
@@ -2,7 +2,6 @@ import 'dart:ui' as ui; // ✅ Fixes missing UI-related symbols
 import 'package:flutter_test/flutter_test.dart';
 import 'package:incubyte/logic.dart';
 
-
 void main() {
   late StringCalculator calculator;
 
@@ -16,5 +15,25 @@ void main() {
     });
 
 
-   });
+    //single number
+
+        test('Single number returns the same number', () {
+      expect(calculator.add('5'), 5);
+    });
+
+    //two numbers separated by comma
+
+        test('Two numbers separated by comma should return their sum', () {
+      expect(calculator.add('1,2'), 3);
+    });
+
+    //multiple numbers separated by comma
+
+        test('Multiple numbers separated by comma should return their sum', () {
+      expect(calculator.add('1,2,3,4,5'), 15);
+    });
+
+
+
+  });
 }
