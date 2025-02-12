a8a9ba1f2a62319a767e221cbcf542ecbb33d9ca
tushar-ventura <48640828+trick4u@users.noreply.github.com>
2025-02-12 15:21:27 +0530

    logicUpdate

--- lib/logic.dart
+++ lib/logic.dart
@@ -1,3 +1,34 @@
+//logic part
 
+import 'package:flutter/material.dart';
 
+class StringCalculator {
+  int add(String numbers) {
+    if (numbers.isEmpty) {
+      return 0;
+    }
+    String delimiter = ',';
+    String numbersToProcess = numbers;
 
+    //custom delimiter
+    if (numbers.startsWith('//')) {
+      var parts = numbers.split('\n');
+      delimiter = parts[0].substring(2);
+      numbersToProcess = parts[1];
+    }
+    numbersToProcess = numbersToProcess.replaceAll('\n', delimiter);
+
+    List<int> nums = numbersToProcess
+        .split(delimiter)
+        .map((str) => int.parse(str.trim()))
+        .toList();
+
+    var negativeNumbers = nums.where((num) => num < 0).toList();
+    if (negativeNumbers.isNotEmpty) {
+      throw Exception(
+          'negative numbers not allowed: ${negativeNumbers.join(',')}'); // Added colon and space
+    }
+
+    return nums.reduce((sum, num) => sum + num);
+  }
+}
--- lib/string_calculator_page.dart
+++ lib/string_calculator_page.dart
@@ -10,8 +10,62 @@ class StringcalUI extends StatefulWidget {
 }
 
 class _StringcalUIState extends State<StringcalUI> {
+  final _controller = TextEditingController();
+  String? _result;
+  String? _error;
+
+  void _calculate() {
+    // try {
+    //   // final result = _calculator.add(_controller.text);
+    //   setState(() {
+    //     _result = result.toString();
+    //     _error = null;
+    //   });
+    // } catch (e) {
+    //   setState(() {
+    //     _error = e.toString();
+    //     _result = null;
+    //   });
+    // }
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text('String Incubyte'),
+      ),
+      body: Container(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              TextField(
+                controller: _controller,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Enter numbers separated by commas',
+                  helperText: 'Press enter to calculate',
+                ),
+                onSubmitted: (_) => _calculate(),
+              ),
+              const SizedBox(height: 16),
+              TextButton(
+                onPressed: _calculate,
+                child: const Text('Calculate'),
+              ),
+              const SizedBox(height: 24),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
